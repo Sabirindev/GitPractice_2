4d2e3cbc21c7642eced4613c107401d286507d83
sabir mustafayev <sdetinscrum@gmail.com>
2021-07-28 12:43:51 -0400

    made some changes

---
+++ fff1/FFF1/Practice55.java
@@ -0,0 +1,7 @@
+package FFF1;
+
+public class Practice55 {
+    public static void main(String[] args) {
+        System.out.println("TamerLan");
+    }
+}
---
+++ fff1/FFF1/Practice66.java
@@ -0,0 +1,7 @@
+package FFF1;
+
+public class Practice66 {
+    public static void main(String[] args) {
+        System.out.println("Sabir");
+    }
+}
